866610b437ae4d3739a81d11c145172d534f6626
Min_Liu <Min_Liu@singaporeair.com.sg>
2025-04-11 16:20:14 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
+import "./page.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        xxx
-      </footer>
+    <div className="root">
+      <Image
+        className="logo"
+        src="/logo.png"
+        alt="Next.js logo"
+        width={343}
+        height={149}
+        priority
+      />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <h1 className="w-full text-center text-[72px] font-bold gradient-text">
+            感恩大抽奖
+          </h1>
+          <div className="box"></div>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          xxx
+        </footer>
+      </div>
     </div>
   );
 }
