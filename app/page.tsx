adc07ec874ac6b9bf5d60fc84b726bd99d90e983
Min_Liu <Min_Liu@singaporeair.com.sg>
2025-04-14 03:10:16 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import React, { useRef } from "react";
 import Image from "next/image";
+import LuckyDraw, { LuckyDrawRef } from "./components/LuckyDraw";
 import "./page.css";
 
 export default function Home() {
+  const childRef = useRef<LuckyDrawRef>(null);
+
+  const handleClick = () => {
+    // 访问子组件暴露的 DOM 或方法
+    childRef.current?.run();
+  };
+
   return (
     <div className="root">
       <Image
@@ -17,7 +28,16 @@ export default function Home() {
           <h1 className="w-full text-center text-[72px] font-bold gradient-text">
             感恩大抽奖
           </h1>
-          <div className="box"></div>
+          <div className="box">
+            <LuckyDraw ref={childRef} />
+            <button
+              onClick={() => {
+                handleClick();
+              }}
+            >
+              start
+            </button>
+          </div>
         </main>
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           xxx
