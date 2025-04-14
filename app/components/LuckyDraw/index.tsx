cce27504cbd0ea84d69af1fb0751c7c98c403b60
Min_Liu <Min_Liu@singaporeair.com.sg>
2025-04-14 11:20:32 +0800

    update

--- app/components/LuckyDraw/index.tsx
+++ app/components/LuckyDraw/index.tsx
@@ -3,6 +3,28 @@
 import { useState, useImperativeHandle, Ref } from "react";
 import styles from "./index.module.css";
 
+function hideSensitiveInfo(str: string) {
+  if (str === "开始") {
+    return str;
+  }
+
+  if (typeof str !== "string") {
+    return str;
+  }
+
+  // 处理姓名：匹配中文姓名（2-4个汉字），保留第一个字，其余替换为*
+  const nameRegex = /([\u4e00-\u9fa5]{2,4})/g;
+  str = str.replace(nameRegex, (match) => {
+    return match[0] + "*".repeat(match.length - 1);
+  });
+
+  // 处理电话号码：将中间4位替换为*
+  const phoneRegex = /(\d{3})(\d{4})(\d{4})/g;
+  str = str.replace(phoneRegex, "$1****$3");
+
+  return str;
+}
+
 // 定义子组件暴露的 API 类型
 export type LuckyDrawRef = {
   run: () => void;
@@ -42,7 +64,12 @@ export default function LuckyDraw({
     },
   }));
 
-  const sssss = [...lotteryList, ...change];
+  const sssss = [
+    ...new Array(120).fill("x").map((i, index) => {
+      return lotteryList[index % lotteryList.length];
+    }),
+    ...change,
+  ].map((i) => hideSensitiveInfo(i));
 
   // const aaa = getMultiRandomInt(0, sssss.length - 1, 4);
 
